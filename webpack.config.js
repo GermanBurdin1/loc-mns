f3d56e1025260a0b62b1311e56944533dbe9c736
German <125369843+GermanBurdin1@users.noreply.github.com>
2024-03-10 21:41:30 +0100

    modified gitignore

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,27 @@
 const path = require('path');
 
 module.exports = {
-  // Точка входа в ваше приложение
-  entry: './src/index.js', // Путь к основному JS файлу
+  entry: './src/index.js', 
 
-  // Конфигурация для модулей
   module: {
     rules: [
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
-      // Вы можете добавить другие правила для других типов файлов здесь
     ],
   },
 
-  // Конфигурация выходных файлов (output)
   output: {
-    path: path.resolve(__dirname, 'dist'), // Путь к папке сборки
-    filename: 'bundle.js', // Имя собранного JS файла
+    path: path.resolve(__dirname, 'dist'), 
+    filename: 'bundle.js',
   },
 
-  // Дополнительные опции, такие как Source Maps
   devtool: 'inline-source-map',
 
-  // Настройки для DevServer (если вы используете его для разработки)
   devServer: {
-    static: './dist', // Папка откуда сервер будет раздавать файлы
-    open: true, // Автоматически открывать браузер
-    hot: true // Включить горячую перезагрузку модулей
+    static: './dist', 
+    open: true, 
+    hot: true
   }
 };
